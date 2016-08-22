a739e46d43bbf42851662afcf9ea4b6e33b82d91
Chetan U <chetan.dec26@gmail.com>
2016-08-22 22:41:57 +0530

    Initial checkin for resto.angular js app with spring mvc

---
+++ Resto/src/main/java/com/resto/gen/service/RestoService.java
@@ -0,0 +1,27 @@
+package com.resto.gen.service;
+
+import java.util.List;
+
+import com.resto.gen.model.User;
+
+
+
+public interface RestoService {
+	
+	User findById(long id);
+	
+	User findByName(String name);
+	
+	void saveUser(User user);
+	
+	void updateUser(User user);
+	
+	void deleteUserById(long id);
+
+	List<User> findAllUsers(); 
+	
+	void deleteAllUsers();
+	
+	public boolean isUserExist(User user);
+	
+}
